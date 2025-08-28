f76baff697e9132a6f1a938d0b60c81f764e4eaa
gustapb77 <gusta9229@gmail.com>
2025-08-28 12:50:02 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -35,5 +35,7 @@ def health():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+   port = int(os.environ.get('PORT', 5001))
+app.run(host='0.0.0.0', port=port, debug=False)
+
 
